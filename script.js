447f27c15f082a781b0bab4af49699583d3ae612
sulimansharadqa <sulimansharadqa@gmail.com>
2024-05-06 22:37:10 +0300

    Website for Assignment

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculation(){
+    var width = parseFloat(document.getElementById('width').value);
+    var height = parseFloat(document.getElementById('height').value);
+    var result;
+
+    if(document.getElementById('area').checked){
+        result = width * height;
+    } else if(document.getElementById('boundary').checked){
+        result = (width + height) * 2;
+    }
+    
+    document.getElementById('result').innerHTML = result || "0.0";
+}
+
+function clearFields(){
+    document.getElementById('stName').value = "";
+    document.getElementById('width').value = "";
+    document.getElementById('height').value = "";
+    document.getElementById('boundary').checked = false;
+    document.getElementById('area').checked = true;
+    document.getElementById('result').innerHTML = "0.0";
+}
